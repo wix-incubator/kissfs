45b6231bd9b8f50eb7403fd5cb9ab915ddac469f
amir-arad <amira@wix.com>
2016-11-16 15:36:00 +0200

    initial empty project

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var loaders = {
+    loaders: [
+        {
+            test: /\.ts[x]?$/,
+            loader: 'ts-loader?logLevel=warn' // &transpileOnly=true
+        }
+    ],
+    noParse: /\.min\.js$/
+};
+
+var resolve = {
+    extensions: [".webpack.js", ".web.js", ".js", ".ts", ".tsx"]
+};
+
+var output = {
+    path: __dirname + '/dist',
+    filename: '[name].bundle.js',
+    libraryTarget: 'umd',
+    library: '[name]',
+    pathinfo: true
+};
+
+module.exports = {
+    context: __dirname,
+    entry: {
+        test: ['./test'],
+        webtest: ['mocha!./test']
+    },
+    devtool: 'inline-source-map',
+    output: output,
+    resolve: resolve,
+    module: loaders
+};
