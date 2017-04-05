851c792aa9d6c1f43df7cde26f28cf3e6d01f592
Yuri Tkachenko <yuri@ya.ru>
2017-04-05 12:50:04 +0300

    sync methods in memeoryFS

--- src/memory-fs.ts
+++ src/memory-fs.ts
@@ -44,48 +44,67 @@ export class MemoryFileSystem implements FileSystem {
         return current;
     }
 
-    saveFile(fullPath:string, newContent:string):Promise<void> {
+    saveFile(fullPath:string, newContent:string): Promise<void> {
+        // return Promise.try(() => this.saveFileSync(fullPath, newContent));
+        return Promise.resolve(this.saveFileSync(fullPath, newContent));
+    }
+
+    deleteFile(fullPath:string): Promise<void> {
+        return Promise.try(() => this.deleteFileSync(fullPath));
+    }
+
+    deleteDirectory(fullPath: string, recursive?: boolean): Promise<void> {
+        return Promise.try(() => this.deleteDirectorySync(fullPath, recursive))
+    }
+
+    ensureDirectory(fullPath:string):Promise<void> {
+        return Promise.try(() => this.ensureDirectorySync(fullPath));
+    }
+
+    loadTextFile(fullPath): Promise<string> {
+        return Promise.try(() => this.loadTextFileSync(fullPath));
+    }
+
+    loadDirectoryTree(): Promise<Directory> {
+        return Promise.resolve(this.loadDirectoryTreeSync());
+    }
+
+    saveFileSync(fullPath:string, newContent:string): void {
         if (this.isIgnored(fullPath)) {
-            return Promise.reject(new Error(`Unable to save ignored path: '${fullPath}'`));
+            throw new Error(`Unable to save ignored path: '${fullPath}'`);
         }
 
         const pathArr = getPathNodes(fullPath);
         const name = pathArr.pop();
-        if (name) {
-            const parentPath = pathArr.join(pathSeparator);
-            return this.ensureDirectory(parentPath)
-                .then(()=> {
-                    const parent = this.getPathTarget(pathArr);
-                    if (parent) {
-                        let type;
-                        const existingChild = find(parent.children, {name});
-                        if (isDir(existingChild)) {
-                            return Promise.reject(new Error(`file save error for path '${fullPath}'`));
-                        } else if (isFile(existingChild)) {
-                            if (existingChild.content === newContent){
-                                return Promise.resolve();
-                            }
-                            existingChild.content = newContent
-                            type = 'fileChanged';
-                        } else {
-                            type = 'fileCreated';
-                            parent.children.push(new File(name, fullPath, newContent))
-                        }
-
-                        this.events.emit(type, {type, fullPath, newContent});
-                        return Promise.resolve();
-                    } else {
-                        // we get here if findNode couldn't resolve the parent and the node is not the root dir.
-                        // this should not happen after running ensureDirectory()
-                        return Promise.reject(new Error(`unexpected error: not a legal file name? '${fullPath}'`));
-                    }
-                });
-        } else {
-            return Promise.reject(new Error(`root is not a legal file name`));
+        if (!name) {
+            throw new Error(`root is not a legal file name`);
         }
+
+        this.ensureDirectorySync(pathArr.join(pathSeparator));
+        const parent = this.getPathTarget(pathArr);
+        if (!parent) {
+            // we get here if findNode couldn't resolve the parent and the node is not the root dir.
+            // this should not happen after running ensureDirectory()
+            throw new Error(`unexpected error: not a legal file name? '${fullPath}'`);
+        }
+
+        let type;
+        const existingChild = find(parent.children, {name});
+        if (isDir(existingChild)) {
+            throw new Error(`file save error for path '${fullPath}'`);
+        }
+        if (isFile(existingChild) && existingChild.content !== newContent) {
+            existingChild.content = newContent
+            type = 'fileChanged';
+        } else {
+            type = 'fileCreated';
+            parent.children.push(new File(name, fullPath, newContent))
+        }
+
+        this.events.emit(type, {type, fullPath, newContent});
     }
 
-    deleteFile(fullPath:string):Promise<void> {
+    deleteFileSync(fullPath:string): void {
         const pathArr = getPathNodes(fullPath);
         const parent = pathArr.length ? this.getPathTarget(pathArr.slice(0, pathArr.length - 1)) : null;
         if (isDir(parent) && !this.isIgnored(fullPath)) {
@@ -94,75 +113,77 @@ export class MemoryFileSystem implements FileSystem {
                 parent.children = parent.children.filter(({name}) => name !== node.name);
                 this.events.emit('fileDeleted', {type: 'fileDeleted', fullPath});
             } else if (isDir(node)){
-                return Promise.reject(new Error(`Directory is not a file '${fullPath}'`));
+                throw new Error(`Directory is not a file '${fullPath}'`);
             }
         }
-        return Promise.resolve();
     }
 
-    deleteDirectory(fullPath: string, recursive?: boolean): Promise<void> {
+    deleteDirectorySync(fullPath: string, recursive?: boolean): void {
         const pathArr = getPathNodes(fullPath);
         if (pathArr.length === 0){
-            return Promise.reject(new Error(`Can't delete root directory`));
+            throw new Error(`Can't delete root directory`);
         }
         const parent = this.getPathTarget(pathArr.slice(0, pathArr.length - 1));
         if (isDir(parent) && !this.isIgnored(fullPath)) {
             const node = find(parent.children, {name: last(pathArr)});
             if (isFile(node)) {
-                return Promise.reject(new Error(`File is not a directory '${fullPath}'`));
+                throw new Error(`File is not a directory '${fullPath}'`);
             } else if(isDir(node)){
                 if (!recursive && node.children.length) {
-                    return Promise.reject(new Error(`Directory is not empty '${fullPath}'`));
+                    throw new Error(`Directory is not empty '${fullPath}'`);
                 } else {
                     parent.children = parent.children.filter(({name}) => name !== node.name);
                     this.recursiveEmitDeletion(node)
                 }
             }
         }
-        return Promise.resolve();
     }
 
-    ensureDirectory(fullPath:string):Promise<void> {
+    ensureDirectorySync(fullPath:string): void {
         if (this.isIgnored(fullPath)) {
-            return Promise.reject(new Error(`Unable to read and write ignored path: '${fullPath}'`));
+            throw new Error(`Unable to read and write ignored path: '${fullPath}'`);
         }
-        return Promise.reduce(getPathNodes(fullPath), (current, name) => {
+
+        getPathNodes(fullPath).reduce((current, name) => {
             const next = find(current.children, {name});
             if (isDir(next)) {
                 return next;
-            } else if (isFile(next)) {
-                return Promise.reject(new Error(`File is not a directory ${next.fullPath}`));
-            } else {
-                const newDir = new Directory(
-                    name,
-                    current.fullPath ? [current.fullPath, name].join(pathSeparator) : name,
-                );
-                current.children.push(newDir)
-                this.events.emit('directoryCreated', {type:'directoryCreated', fullPath:newDir.fullPath});
-                return newDir;
             }
-        }, this.root).return();
+            if (isFile(next)) {
+                throw new Error(`File is not a directory ${next.fullPath}`);
+            }
+            const newDir = new Directory(
+                name,
+                current.fullPath ? [current.fullPath, name].join(pathSeparator) : name,
+            );
+            current.children.push(newDir)
+            this.events.emit('directoryCreated', {
+                type: 'directoryCreated',
+                fullPath: newDir.fullPath
+            });
+            return newDir;
+        }, this.root)
     }
 
-    loadTextFile(fullPath): Promise<string> {
+    loadTextFileSync(fullPath): string {
         if (this.isIgnored(fullPath)) {
-            return Promise.reject(new Error(`Unable to read ignored path: '${fullPath}'`));
+            throw new Error(`Unable to read ignored path: '${fullPath}'`);
         }
         const pathArr = getPathNodes(fullPath);
-        const parent = (pathArr.length) ? this.getPathTarget(pathArr.slice(0, pathArr.length - 1)) : null;
+        const parent = pathArr.length ? this.getPathTarget(pathArr.slice(0, pathArr.length - 1)) : null;
         if (isDir(parent)) {
             const node = find(parent.children, {name: last(pathArr)});
             if (isFile(node)) {
-                return Promise.resolve(node.content || '');
+                return node.content || '';
             } else if (isDir(node)) {
-                return Promise.reject(new Error(`File is a directory ${fullPath}`));
+                throw new Error(`File is a directory ${fullPath}`);
             }
         }
-        return Promise.reject(new Error(`Cannot find file ${fullPath}`));
+        throw new Error(`Cannot find file ${fullPath}`);
     }
 
-    loadDirectoryTree(): Promise<Directory> {
-        return Promise.resolve(this.parseTree(this.root) as Directory);
+    loadDirectoryTreeSync(): Directory {
+        return this.parseTree(this.root) as Directory
     }
 
     private parseTree(treeRoot: FileSystemNode): FileSystemNode {
