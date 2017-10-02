a4b2782746c5f4cba89265e948e576fb263a79e4
Avi Vahl <avi.vahl@wix.com>
2017-10-02 19:29:23 +0300

    Add sinon-chai for stubs/spies assertions

--- test-kit/index.ts
+++ test-kit/index.ts
@@ -1,6 +1,8 @@
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
+import * as sinonChai from 'sinon-chai';
 import chaiSubset = require('chai-subset');
 
 chai.use(chaiSubset);
+chai.use(sinonChai);
 chai.use(chaiAsPromised);
