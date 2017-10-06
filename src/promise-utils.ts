748d64d5446867c44550c90fec7c09eef740a757
Avi Vahl <avi.vahl@wix.com>
2017-10-06 05:43:42 +0300

    Move timeout handling to try and use symbol This way, avoids always creating a rejected timeout Promise, even if operation succeeded.

--- src/promise-utils.ts
+++ src/promise-utils.ts
@@ -16,7 +16,7 @@ export interface RetryPromiseOptions {
     timeoutMessage?: string;
 }
 
-const uniqueObj = {}; // used to identify timeout in retryPromise
+const timeoutSymbol = Symbol('timeout');
 
 export async function retryPromise<T>(
     promiseProvider: () => Promise<T>,
@@ -25,24 +25,24 @@ export async function retryPromise<T>(
         throw new Error(`timeout (${timeout}ms) must be greater than retries (${retries}) times interval (${interval}ms)`);
     }
     let lastError: Error | undefined;
-    const timeoutReject = timeout && delayedPromise(timeout).then(() => Promise.reject(uniqueObj));
+    const timeoutPromise = timeout && delayedPromise(timeout).then(() => timeoutSymbol);
     do {
         const shouldDelay = interval && lastError !== undefined;
         try {
-            if (timeoutReject) {
-                shouldDelay && await Promise.race([delayedPromise(interval), timeoutReject]);
-                return await Promise.race([promiseProvider(), timeoutReject])
+            if (timeoutPromise) {
+                shouldDelay && await Promise.race([delayedPromise(interval), timeoutPromise]);
+                const result = await Promise.race([promiseProvider(), timeoutPromise])
+                if (result === timeoutSymbol) {
+                    lastError = lastError || new Error(timeoutMessage);
+                    break;
+                }
+                return result as T;
             } else {
                 shouldDelay && await delayedPromise(interval);
                 return await promiseProvider();
             }
         } catch (e) {
-            if (e === uniqueObj) { // only retry if not a timeout
-                retries = 0;
-                lastError = lastError || new Error(timeoutMessage);
-            } else {
-                lastError = e;
-            }
+            lastError = e;
         }
     } while (retries-- > 0);
     throw lastError;
