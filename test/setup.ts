6845b255d705e096320e3ce0e36a639de16e1c63
Avi Vahl <avi.vahl@wix.com>
2018-09-18 13:38:46 +0300

    Change import = require() to import default statements TypeScript has esModuleInterop turned on by default these days, and so do we.

--- test/setup.ts
+++ test/setup.ts
@@ -1,7 +1,7 @@
-import chai = require("chai");
-import chaiAsPromised = require("chai-as-promised");
-import sinonChai = require("sinon-chai");
-import chaiSubset = require("chai-subset");
+import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+import sinonChai from "sinon-chai";
+import chaiSubset from "chai-subset";
 
 chai.use(chaiSubset);
 chai.use(sinonChai);
