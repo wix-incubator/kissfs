9c7a4ac80b34c1a667586f7d09f28ccd827c9282
Avi Vahl <avi.vahl@wix.com>
2018-10-18 00:45:51 +0300

    Simplify webpack test config

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,25 @@
 const path = require('path');
-const glob = require("glob");
-const NODE_MODULES_PATH = path.resolve(__dirname, 'node_modules');
-const polyfills = ['core-js/es6/array', 'core-js/es6/number', 'core-js/es6/promise', 'core-js/es6/symbol'];
-const testsSetup = `mocha-loader!${path.join(__dirname, 'test', 'setup.ts')}`;
-const universalTestFiles = glob.sync(path.join(__dirname, 'test', '**', '*.spec.ts?(x)'));
-// const browserTestFiles = glob.sync(path.join(__dirname, 'test', '**', '*.spec.browser.ts?(x)'));
+const glob = require('glob');
 
 module.exports = {
+    context: __dirname,
     mode: 'development',
     devtool: 'source-map',
-    context: __dirname,
     entry: {
-        test: [...polyfills, testsSetup, ...universalTestFiles]
+        test: [
+            `mocha-loader!${path.join(__dirname, 'test', 'setup.ts')}`,
+            ...glob.sync(path.join(__dirname, 'test', '**', '*.spec.ts'))
+        ]
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 loader: '@ts-tools/webpack-loader',
-                exclude : NODE_MODULES_PATH
+                exclude : path.join(__dirname, 'node_modules')
             }
         ],
         noParse: /\.min\.js$/
-    },
-    output: {
-        filename: '[name].bundle.js',
-        libraryTarget: 'umd',
-        library: '[name]'
     },
     resolve: {
         extensions: ['.ts', '.js', '.json']
