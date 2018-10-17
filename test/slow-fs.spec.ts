50a63253890e33f8e2edef7f4f68adee404cfd39
Avi Vahl <avi.vahl@wix.com>
2018-10-18 00:39:16 +0300

    Convert slow and timeout fs tests to async/await easier to read/debug

--- test/slow-fs.spec.ts
+++ test/slow-fs.spec.ts
@@ -1,11 +1,12 @@
-import {expect} from 'chai';
-import {FileSystem} from '../src/api';
-import {assertFileSystemContract, content, dirName, fileName} from '../test/implementation-suite';
-import {SlowFs} from './slow-fs';
+import { expect } from 'chai';
+import { FileSystem } from '../src/api';
+import { assertFileSystemContract, content, dirName, fileName } from '../test/implementation-suite';
+import { SlowFs } from './slow-fs';
 
 describe('the slow (delayed) file system implementation', () => {
     const delay = 40;
     const accuracyFactor = 0.9;
+
     assertFileSystemContract(async () => new SlowFs(delay), {
         retries: 15,
         interval: 2,
@@ -19,23 +20,25 @@ describe('the slow (delayed) file system implementation', () => {
 
         beforeEach(() => {
             startTimestamp = Date.now();
-            return Promise.resolve(new SlowFs(delay)).then(newFs => fs = newFs);
+            fs = new SlowFs(delay);
         });
 
-        it(`delay the dir creation, reading tree and deleting`, () => {
-            return fs.ensureDirectory(dirName)
-                .then(() => fs.loadDirectoryTree())
-                .then(() => fs.stat(dirName))
-                .then(() => fs.deleteDirectory(dirName))
-                .then(() => expect(Date.now() - startTimestamp).to.be.at.least(delay * 4 * accuracyFactor));
+        it(`delay the dir creation, reading tree and deleting`, async () => {
+            await fs.ensureDirectory(dirName);
+            await fs.loadDirectoryTree();
+            await fs.stat(dirName);
+            await fs.deleteDirectory(dirName);
+
+            expect(Date.now() - startTimestamp).to.be.at.least(delay * 4 * accuracyFactor);
         });
 
-        it(`delay the file saving, reading and deleting`, () => {
-            return fs.saveFile(fileName, content)
-                .then(() => fs.loadTextFile(fileName))
-                .then(() => fs.stat(fileName))
-                .then(() => fs.deleteFile(fileName))
-                .then(() => expect(Date.now() - startTimestamp).to.be.at.least(delay * 4 * accuracyFactor));
+        it(`delay the file saving, reading and deleting`, async () => {
+            await fs.saveFile(fileName, content);
+            await fs.loadTextFile(fileName);
+            await fs.stat(fileName);
+            await fs.deleteFile(fileName);
+
+            expect(Date.now() - startTimestamp).to.be.at.least(delay * 4 * accuracyFactor);
         });
     });
 });
--- test/timeout-fs.spec.ts
+++ test/timeout-fs.spec.ts
@@ -1,14 +1,14 @@
-import {expect} from 'chai';
-import {assertFileSystemContract, dirName, fileName} from './implementation-suite';
-import {SlowFs} from './slow-fs';
-import {FileSystem, MemoryFileSystem, TimeoutFileSystem} from '../src/universal';
+import { expect } from 'chai';
+import { assertFileSystemContract, dirName, fileName } from './implementation-suite';
+import { SlowFs } from './slow-fs';
+import { FileSystem, MemoryFileSystem, TimeoutFileSystem } from '../src/universal';
 
 describe('the timeout file system proxy', () => {
     const timeout = 200;
 
     assertFileSystemContract(() =>
-            Promise.resolve(new TimeoutFileSystem(timeout, new MemoryFileSystem())),
-        {retries: 15, interval: 2, timeout: 40, noExtraEventsGrace: 10}
+        Promise.resolve(new TimeoutFileSystem(timeout, new MemoryFileSystem())),
+        { retries: 15, interval: 2, timeout: 40, noExtraEventsGrace: 10 }
     );
 
     describe(`delayed timeout test`, () => {
@@ -21,39 +21,39 @@ describe('the timeout file system proxy', () => {
             fs = new TimeoutFileSystem(timeout, new SlowFs(delay));
         });
 
-        it(`ensureDirectory exit before delay is over`, () => {
-            return expect(fs.ensureDirectory(dirName)).to.eventually.be.rejectedWith('timed out')
-                .then(() => expect(startTimestamp - Date.now()).to.be.below(delay));
+        it(`ensureDirectory exit before delay is over`, async () => {
+            await expect(fs.ensureDirectory(dirName)).to.eventually.be.rejectedWith('timed out');
+            expect(startTimestamp - Date.now()).to.be.below(delay);
         });
 
-        it(`saveFile exit before delay is over`, () => {
-            return expect(fs.saveFile(`${dirName}\\${fileName}`, '#goodnessSquad')).to.eventually.be.rejectedWith('timed out')
-                .then(() => expect(startTimestamp - Date.now()).to.be.below(delay));
+        it(`saveFile exit before delay is over`, async () => {
+            await expect(fs.saveFile(`${dirName}\\${fileName}`, '#goodnessSquad')).to.eventually.be.rejectedWith('timed out');
+            expect(startTimestamp - Date.now()).to.be.below(delay);
         });
 
-        it(`deleteFile exit before delay is over`, () => {
-            return expect(fs.deleteFile(`${dirName}\\${fileName}`)).to.eventually.be.rejectedWith('timed out')
-                .then(() => expect(startTimestamp - Date.now()).to.be.below(delay));
+        it(`deleteFile exit before delay is over`, async () => {
+            await expect(fs.deleteFile(`${dirName}\\${fileName}`)).to.eventually.be.rejectedWith('timed out');
+            expect(startTimestamp - Date.now()).to.be.below(delay);
         });
 
-        it(`deleteDirectory exit before delay is over`, () => {
-            return expect(fs.deleteDirectory(dirName)).to.eventually.be.rejectedWith('timed out')
-                .then(() => expect(startTimestamp - Date.now()).to.be.below(delay));
+        it(`deleteDirectory exit before delay is over`, async () => {
+            await expect(fs.deleteDirectory(dirName)).to.eventually.be.rejectedWith('timed out');
+            expect(startTimestamp - Date.now()).to.be.below(delay);
         });
 
-        it(`loadTextFile exit before delay is over`, () => {
-            return expect(fs.loadTextFile(dirName)).to.eventually.be.rejectedWith('timed out')
-                .then(() => expect(startTimestamp - Date.now()).to.be.below(delay));
+        it(`loadTextFile exit before delay is over`, async () => {
+            await expect(fs.loadTextFile(dirName)).to.eventually.be.rejectedWith('timed out');
+            expect(startTimestamp - Date.now()).to.be.below(delay);
         });
 
-        it(`loadDirectoryTree exit before delay is over`, () => {
-            return expect(fs.loadDirectoryTree()).to.eventually.be.rejectedWith('timed out')
-                .then(() => expect(startTimestamp - Date.now()).to.be.below(delay));
+        it(`loadDirectoryTree exit before delay is over`, async () => {
+            await expect(fs.loadDirectoryTree()).to.eventually.be.rejectedWith('timed out');
+            expect(startTimestamp - Date.now()).to.be.below(delay);
         });
 
-        it(`stat exit before delay is over`, () => {
-            return expect(fs.stat(dirName)).to.eventually.be.rejectedWith('timed out')
-                .then(() => expect(startTimestamp - Date.now()).to.be.below(delay));
+        it(`stat exit before delay is over`, async () => {
+            await expect(fs.stat(dirName)).to.eventually.be.rejectedWith('timed out');
+            expect(startTimestamp - Date.now()).to.be.below(delay);
         });
     });
 });
